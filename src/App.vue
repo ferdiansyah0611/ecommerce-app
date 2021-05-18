20a00ddfbc0c977e5ede13643fc77db5cf61750b
Ferdiansyah <ferdif9996@gmail.com>
2021-05-18 17:50:16 +0700

    update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+	<Navbar></Navbar>
+	<div class="p-3 mt-14">
+		<router-view/>
+	</div>
+	<div class="flex bg-gray-800 p-2 text-white">
+		<div class="sm:w-full md:w-1/2">
+			<p>&copy; {{new Date().getFullYear()}} <a href="/" class="hover:underline">ferdiansyah0611</a></p>
+		</div>
+		<div class="sm:w-full md:w-1/2">
+			
+		</div>
+	</div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import Navbar from '@/components/Navbar'
+export default{
+	components: {
+		Navbar
+	}
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
