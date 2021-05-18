b159b8781e597663b6dcf541f8998af821b446b1
Ferdiansyah <ferdif9996@gmail.com>
2021-05-18 17:49:59 +0700

    import store vuex

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import App from './App.vue'
 import './assets/tailwind.css'
 import {auth} from '@/firebase'
 import router from './router'
+import store from './store'
 
 var app
 auth.onAuthStateChanged(() => {
 	if(!app){
-		createApp(App).use(router).use(router).mount('#app')
+		createApp(App).use(store).use(router).use(router).mount('#app')
 	}
 })
