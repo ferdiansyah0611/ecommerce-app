927991722c0bce23213a0ef1864674d6672b1af2
Ferdiansyah <ferdif9996@gmail.com>
2021-05-18 17:48:27 +0700

    new routing show products

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,11 @@ const routes = [
     component: Home
   },
   {
+    path: '/products/:id',
+    name: 'ShowProduct',
+    component: () => import('../views/ShowProduct.vue')
+  },
+  {
     path: '/about',
     name: 'About',
     // route level code-splitting
