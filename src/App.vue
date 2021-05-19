acd7c650805fe8c05a19e7d941b94f9aab98593c
Ferdiansyah <ferdif9996@gmail.com>
2021-05-19 17:12:27 +0700

    import Footer in App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,15 @@
 	<div class="p-3 mt-14">
 		<router-view/>
 	</div>
-	<div class="flex bg-gray-800 p-2 text-white">
-		<div class="sm:w-full md:w-1/2">
-			<p>&copy; {{new Date().getFullYear()}} <a href="/" class="hover:underline">ferdiansyah0611</a></p>
-		</div>
-		<div class="sm:w-full md:w-1/2">
-			
-		</div>
-	</div>
+	<Footer></Footer>
 </template>
 <script>
 import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 export default{
 	components: {
-		Navbar
+		Navbar,
+		Footer
 	}
 }
 </script>
