f5f0f58c4399d98f28050f38acb3c53f7d37dc7d
Ferdiansyah <ferdif9996@gmail.com>
2021-05-19 17:14:33 +0700

    update router & store

--- src/router/index.js
+++ src/router/index.js
@@ -13,12 +13,14 @@ const routes = [
     component: () => import('../views/ShowProduct.vue')
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/categories/:id',
+    name: 'Category',
+    component: () => import('../views/ShowProduct.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
   }
 ]
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex'
+import {db, storage} from '@/firebase'
 
 export default createStore({
   state: {
@@ -17,6 +18,36 @@ export default createStore({
     },
     addData(state, data){
       state[data.name] = [...state[data.name], data.data]
+    },
+    createProduct(state, data){
+      let add, filePreview, listFilePreview = []
+      add = db.collection('products').doc()
+      filePreview = () => {
+        if(data.imgPreview.length >= 1){
+          data.imgPreview.forEach((data) => {
+            const upload = storage.ref(`/images/${data.name}`).put(data)
+            upload.on('state_changed', status => {
+              var progress = (status.bytesTransferred / status.totalBytes) * 100;
+              console.log('Progress ' + progress + '%')
+            }, e => console.log(e), () => {
+              upload.snapshot.ref.getDownloadURL().then(function(url) {
+                listFilePreview.push(url)
+              })
+            })
+          })
+        }
+      }
+      filePreview()
+      add.set({
+        id: add.id,
+        name: data.name,
+        price: data.price,
+        stock: data.stock,
+        img: data.img,
+        categories: data.categories,
+        imgPreview: listFilePreview,
+        created: db.FieldValue.serverTimestamp()
+      })
     }
   },
   actions: {
