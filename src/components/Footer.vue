f23e7b392a1e3d8e5184955db652111d2b90f180
Ferdiansyah <ferdif9996@gmail.com>
2021-05-19 17:11:56 +0700

    create components Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+	<div class="flex flex-wrap bg-white p-5 text-black mb-12">
+		<div class="w-full md:w-1/2">
+			<p>&copy; {{new Date().getFullYear()}} <a href="/" class="hover:underline">ferdiansyah0611</a></p>
+		</div>
+		<div class="w-full md:w-1/2 flex sm:justify-end">
+			<a href="/">Privacy & Policy</a>
+		</div>
+	</div>
+</template>
