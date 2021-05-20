1e83e2f4645bb439c63d6b83b2ea87663548d31e
Ferdiansyah <ferdif9996@gmail.com>
2021-05-20 18:41:35 +0700

    create view Dashboard.vue

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<div class="border-b pb-2">
+			<h3 class="font-bold text-2xl">Dashboard Pages</h3>
+		</div>
+		<div class="bg-white w-full p-4 flex">
+			<router-link class="bg-blue-400 p-3 text-white" to="/product/create">Create Produts</router-link>
+		</div>
+	</div>
+</template>
+<script>
+export default{
+	name: 'Dashbbard',
+}
+</script>
